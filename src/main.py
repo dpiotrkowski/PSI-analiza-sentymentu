d91d96208897188cd50c117a023475d5826871d2
dpiotrkowski <d.piotrkowsk@student.uw.edu.pl>
2025-05-13 22:44:35 +0200

    usprawniony parser komend

--- src/main.py
+++ src/main.py
@@ -1,32 +1,34 @@
 import argparse
 import subprocess
 
+
 def run_script(script_name, *args):
     """Uruchom dany skrypt z argumentami"""
-    command = ['python', script_name] + list(args)
+    command = ["python", script_name] + list(args)
     subprocess.run(command)
+
 
 def main():
     parser = argparse.ArgumentParser(description="Command runner dla scraperow.")
-    subparsers = parser.add_subparsers(dest='command')
+    subparsers = parser.add_subparsers(dest="command")
 
     # Subparser dla reddit.py
-    reddit_parser = subparsers.add_parser('reddit', help='Uruchom scraper Reddita')
+    subparsers.add_parser("reddit", help="Uruchom scraper Reddita")
     # Subparser dla wykop.py
-    wykop_parser = subparsers.add_parser('wykop', help='Uruchom scraper Wykop ')
+    subparsers.add_parser("wykop", help="Uruchom scraper Wykop ")
 
     args = parser.parse_args()
 
-    if args.command == 'reddit':
+    if args.command == "reddit":
         subreddits = input("Podaj szukane subreddity (rozdzielone przecinkiem): ")
         search_words = input("Podaj szukane słowa (rozdzielone przecinkiem): ")
-        run_script('scrapers/reddit.py', subreddits, search_words)
-    elif args.command == 'wykop':
+        run_script("scrapers/reddit.py", subreddits, search_words)
+    elif args.command == "wykop":
         wykop_tags = input("Podaj tagi z Wykopa (rozdzielone przecinkiem): ")
-        run_script('scrapers/wykop.py', wykop_tags)
+        run_script("scrapers/wykop.py", wykop_tags)
     else:
         parser.print_help()
 
-if __name__ == '__main__':
-    main()
 
+if __name__ == "__main__":
+    main()
