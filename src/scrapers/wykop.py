495779c0103a6da5c12ac5030378ec042238141b
dpiotrkowski <d.piotrkowsk@student.uw.edu.pl>
2025-05-15 21:12:49 +0200

    fix: poprawne zapisywanie do pliku dla wiekszej ilosci tagow

--- src/scrapers/wykop.py
+++ src/scrapers/wykop.py
@@ -24,32 +24,33 @@ if len(sys.argv) < 2:
 wykop_tags = sys.argv[1].split(',')
 
 # Pobieranie ARTYKUŁÓW z tagiem
-for tag in wykop_tags:
-    tag = tag.strip() # Usunięcie białych znaków
-    try:
-        result = client.articles_list_articles_by_tag(  
-            tag=tag,
-            sort=StreamSortType.ALL,
-            limit=100
-        )
-    except Exception as e:
-        print(f"Błąd pobierania danych dla tagu '{tag}': {e}")
-        exit()
+with open("../data/results_wykop.txt", "w", encoding="utf-8") as f:
+    for tag in wykop_tags:
+        tag = tag.strip() # Usunięcie białych znaków
+        try:
+            result = client.articles_list_articles_by_tag(  
+                tag=tag,
+                sort=StreamSortType.ALL,
+                limit=300
+            )
+        except Exception as e:
+            print(f"Błąd pobierania danych dla tagu '{tag}': {e}")
+            exit()
 
-    with open("../data/results_wykop.txt", "w", encoding="utf-8") as f:
         if not result.get('data'):
             print(f"Brak artykułów dla tagu {tag}\n")
             continue 
             
         for article in result['data']:
             try:
-                content = article.get('content', 'BRAK TREŚCI')
+                content = article.get('content')
                 
-                f.write(
-                    f"Tag: {tag}\n"
-                    f"Treść: {content}\n"
-                    f"{'-'*50}\n\n"
-                )
+                if content and content != 'None':
+                    f.write(
+                        f"#{tag}\n"
+                        f"{content}\n"
+                        f"{'-'*50}\n\n"
+                    )
             except Exception as e:
                 print(f"Błąd przetwarzania artykułu: {e}")
                 continue
