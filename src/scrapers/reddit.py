130db331965ee735e7aa4cc00ecf8481aaab9c02
dpiotrkowski <d.piotrkowsk@student.uw.edu.pl>
2025-05-15 21:41:05 +0200

    feat: zmiana z write na append mode by ograniczyć ilość requestow do API

--- src/scrapers/reddit.py
+++ src/scrapers/reddit.py
@@ -30,19 +30,19 @@ search_words = sys.argv[2].split(",")  # Pobranie słów do wyszukiwania z argum
 
 # Krok 3: ZAPIS WYNIKÓW do txt
 os.makedirs("../data", exist_ok=True)
-with open("../data/results_reddit.txt", "w", encoding="utf-8") as reddit_file:
+with open("../data/results_reddit.txt", "a", encoding="utf-8") as reddit_file:
     for subreddit_name in subreddits:
         subreddit_name = subreddit_name.strip()
         subreddit = reddit.subreddit(subreddit_name)
 
-        reddit_file.write(f"Subreddit: {subreddit_name}\n")
+        reddit_file.write(f"r/{subreddit_name}\n")
 
-        for submission in subreddit.search(search_words, sort="new", limit=100):
-            reddit_file.write(f"Tytuł: {submission.title}\n")
+        for submission in subreddit.search(search_words, sort="new", limit=250):
+            reddit_file.write(f"{submission.title}\n")
             # reddit_file.write(f"Wynik: {submission.score}\n")  # opcjonalne
             # reddit_file.write(f"URL: {submission.url}\n")  # opcjonalne
             # OPCJONALNE: Timestampy mogą się przydać do analizy sentymentu w czasie
             # reddit_file.write(f"Utworzono: {submission.created_utc}\n")
-            reddit_file.write(f"Treść: {submission.selftext}\n\n")
+            reddit_file.write(f"{submission.selftext}\n\n")
 
         reddit_file.write("\n" + "-" * 50 + "\n\n")  # Separator między subreddits
